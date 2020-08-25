0329f3eb8a7e6fb3df956163e6dbbc7c19956df0
Volodymyr Silitskyi <volodymyr_silitskyi@epam.com>
2020-08-25 12:49:41 +0300

    first

---
+++ src/test/java/Hello2.java
@@ -0,0 +1,11 @@
+/**
+ * Created by : Volodymyr_Silitskyi
+ * Created at : 8/25/2020
+ */
+
+
+public class Hello2 {
+    public static void main(String[] args) {
+        System.out.println("Hello2");
+    }
+}
